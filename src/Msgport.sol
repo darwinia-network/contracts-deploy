106fd762e61d0cfba2422def2fa45c2b5eac3013
echo <hujw77@gmail.com>
2024-03-25 17:45:20 +0800

    remappings

--- src/Msgport.sol
+++ src/Msgport.sol
@@ -2,12 +2,12 @@
 pragma solidity 0.8.17;
 
 // Subapi
-import {SubAPI} from "@subapi/src/SubAPI.sol";
+import {SubAPI} from "subapi/src/SubAPI.sol";
 
 // ORMP
-import {ORMP} from "@ORMP/src/ORMP.sol";
-import {Oracle} from "@ORMP/src/eco/Oracle.sol";
-import {Relayer} from "@ORMP/src/eco/Relayer.sol";
+import {ORMP} from "ORMP/src/ORMP.sol";
+import {Oracle} from "ORMP/src/eco/Oracle.sol";
+import {Relayer} from "ORMP/src/eco/Relayer.sol";
 
 // Msgport
 import {ORMPUpgradeablePort} from "@darwinia-msgport/src/ports/ORMPUpgradeablePort.sol";
