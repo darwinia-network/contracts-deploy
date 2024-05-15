b9f1128f144b3088247cf9f65001b14f32141807
echo <hujw77@gmail.com>
2024-05-15 16:02:32 +0800

    config

--- script/Connect.s.sol
+++ script/Connect.s.sol
@@ -50,7 +50,7 @@ contract ConnectScript is Base, OracleConfig, RelayerConfig {
     }
 
     function run() public sphinx {
-        bool IS_PROD = vmSafe.envOr("IS_PROD", true);
+        bool IS_PROD = vmSafe.envOr("IS_PROD", false);
         if (IS_PROD) {
             networks = sphinxConfig.mainnets;
         } else {
