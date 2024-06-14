1c0206115023fd77840e8ffa5b8620dae0abea0c
echo <hujw77@gmail.com>
2024-06-14 10:12:25 +0800

    ring dao

---
+++ script/ring-dao/Deploy.s.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Base} from "../common/Base.sol";
+import {TomlTools} from "../common/TomlTools.sol";
+import {stdJson} from "forge-std/StdJson.sol";
