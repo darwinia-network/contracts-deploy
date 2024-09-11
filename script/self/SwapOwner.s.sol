37b1d8c0f996be1c14639a8526ff3d693b0f12b0
echo <hujw77@gmail.com>
2024-09-11 15:01:53 +0800

    Swap owner (#16)
    * swap owner
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * executed

---
+++ script/self/SwapOwner.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Base} from "../common/Base.sol";
+
+interface ISafe {
+    function swapOwner(address prevOwner, address oldOwner, address newOwner) external;
+}
+
+contract SwapOwnerScript is Base {
+    address aki = 0x53405FB4d71591E33fe07bFbC90bD82E65720ad0;
+    address bear = 0x5b7544b3f6aBd9E03Fba494796B1eE6F9543E2e4;
+
+    function run() public sphinx {
+        address self = safeAddress();
+
+        ISafe(self).swapOwner(0x52386BE2397e8EAc26298F733b390684203fB580, aki, bear);
+    }
+}
