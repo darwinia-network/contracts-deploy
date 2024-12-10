1490d3eae5865ab14ea43de6d28271dbc72217f6
echo <hujw77@gmail.com>
2024-12-10 15:47:26 +0800

    swap

--- script/Proposal.s.sol
+++ script/Proposal.s.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "./x-account/Deploy.s.sol";
+// import "./x-account/Deploy.s.sol";
+import "./self/SwapOwner.s.sol";
 
-contract Proposal is DeployScript {}
+contract Proposal is SwapOwnerScript {
+    function run() public sphinx {
+        _run();
+    }
+}
