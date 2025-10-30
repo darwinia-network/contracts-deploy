d7ea3a8a53c3c17158fdf895d52b4264285c83f6
echo <hujw77@gmail.com>
2025-10-30 10:48:30 +0800

    swap guantong to xiaoch

--- script/common/Base.sol
+++ script/common/Base.sol
@@ -18,7 +18,8 @@ abstract contract Base is Script, Sphinx, SphinxConstants {
     function configureSphinx() public override {
         sphinxConfig.projectName = "RING-DAO";
         sphinxConfig.testnets = ["sepolia", "arbitrum_sepolia"];
-        sphinxConfig.mainnets = ["arbitrum", "base", "blast", "crab", "darwinia", "ethereum", "polygon", "moonbeam", "morph"];
+        sphinxConfig.mainnets =
+            ["arbitrum", "base", "blast", "crab", "darwinia", "ethereum", "polygon", "moonbeam", "morph"];
     }
 
     function CREATE3() public returns (address create3) {
--- script/self/SwapOwner2.s.sol
+++ script/self/SwapOwner2.s.sol
@@ -7,16 +7,16 @@ interface ISafe {
     function swapOwner(address prevOwner, address oldOwner, address newOwner) external;
 }
 
-intergace IRelayer {
-	function isApproved(address) external view returns (bool);
-	function setApproved(address, bool) external;
+interface IRelayer {
+    function isApproved(address) external view returns (bool);
+    function setApproved(address, bool) external;
 }
 
 contract SwapOwner2Script is Base {
     address guantong = 0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85;
     address xiaoch = 0x88a39B052d477CfdE47600a7C9950a441Ce61cb4;
 
-	address relayer = 0x114890eB7386F94eae410186F20968bFAf66142a;
+    address relayer = 0x114890eB7386F94eae410186F20968bFAf66142a;
 
     function _run() internal virtual {
         address self = safeAddress();
@@ -24,7 +24,7 @@ contract SwapOwner2Script is Base {
         ISafe(self).swapOwner(0x5b7544b3f6aBd9E03Fba494796B1eE6F9543E2e4, guantong, xiaoch);
 
         if (IRelayer(relayer).isApproved(guantong)) {
-            IRelayer(relyer).setApproved(guantong, false);
+            IRelayer(relayer).setApproved(guantong, false);
         }
     }
 }
